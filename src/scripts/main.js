44500c074b51277e27a372f0600f408f71ccec17
Evg3n22 <evgen.gulevaty@gmail.com>
2025-12-25 16:10:44 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
 // write code here
+const tree = document.querySelector('.tree');
+
+tree.addEventListener('click', (e) => {
+  const list = e.target.querySelector('ul');
+
+  if (list) {
+    list.hidden = !list.hidden;
+  }
+});
